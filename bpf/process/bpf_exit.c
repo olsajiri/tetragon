1e989ed71d1f016f3837f108e3de30364055e645
Jiri Olsa <jolsa@kernel.org>
2022-11-29 15:53:26 +0100

    tetragon: Switch exit tracepoint to do_task_dead kprobe
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/bpf_exit.c
+++ bpf/process/bpf_exit.c
@@ -1,17 +1,34 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright Authors of Cilium */
 
+#include "vmlinux.h"
 #include "bpf_exit.h"
 
 char _license[] __attribute__((section("license"), used)) = "GPL";
 
-__attribute__((section("tracepoint/sys_exit"), used)) int
-event_exit(struct sched_execve_args *ctx)
+static int list_empty(struct list_head *head)
 {
-	__u64 pid_tgid;
+	struct list_head *next;
 
-	pid_tgid = get_current_pid_tgid();
+	probe_read(&next, sizeof(next), _(&head->next));
+	return next == head;
+}
 
-	event_exit_send(ctx, pid_tgid);
+__attribute__((section("kprobe/do_task_dead"), used)) int
+event_exit(struct pt_regs *ctx)
+{
+	struct task_struct *task, *group_leader;
+	struct list_head *thread_group;
+	__u64 tgid;
+
+	tgid = get_current_pid_tgid() >> 32;
+
+	task = (struct task_struct *)get_current_task();
+	probe_read(&group_leader, sizeof(group_leader), _(&task->group_leader));
+	probe_read(&thread_group, sizeof(thread_group),
+		   _(&group_leader->thread_group));
+
+	if (list_empty(thread_group))
+		event_exit_send(ctx, tgid);
 	return 0;
 }
