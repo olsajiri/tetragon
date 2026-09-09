52a749a13c82674a8fc216a2205491dc89ddf104
Jiri Olsa <jolsa@kernel.org>
2026-09-09 08:36:57 +0000

    tetragon: Switch string_prefix_maps_heap map to hash for uprobe/usdt
    The uprobe/usdt probes path in kernel do not disable preemption,
    we need to use hash instead of per-cpu heap.
    
    The uprobe and usdt sensor now create string_prefix_maps_heap as hash
    map with BPF_F_NO_PREALLOC and use it as shared map with 32768 entries.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/generic_maps.h
+++ bpf/process/generic_maps.h
@@ -54,6 +54,8 @@ FUNC_INLINE bool heap_update(heap_key_t key)
 		return false;
 	if (map_update_elem(&string_maps_heap, &key, ro, BPF_ANY))
 		return false;
+	if (map_update_elem(&string_prefix_maps_heap, &key, ro, BPF_ANY))
+		return false;
 	return true;
 }
 
@@ -145,6 +147,7 @@ FUNC_INLINE long heap_dtor(long ret)
 	map_delete_elem(&process_call_heap, &key);
 	map_delete_elem(&buffer_heap_map, &key);
 	map_delete_elem(&string_maps_heap, &key);
+	map_delete_elem(&string_prefix_maps_heap, &key);
 	return ret;
 }
 
