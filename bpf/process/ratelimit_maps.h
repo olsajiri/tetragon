5699ed83785c3db0dea41d75525c3c6084cebb89
Jiri Olsa <jolsa@kernel.org>
2026-09-09 08:46:00 +0000

    tetragon: Switch ratelimit_heap map to hash for uprobe/usdt
    The uprobe/usdt probes path in kernel do not disable preemption,
    we need to use hash instead of per-cpu heap.
    
    The uprobe and usdt sensor now create ratelimit_heap as hash
    map with BPF_F_NO_PREALLOC and use it as shared map with 32768
    entries.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/ratelimit_maps.h
+++ bpf/process/ratelimit_maps.h
@@ -44,11 +44,48 @@ struct {
 
 // The value has extra headroom to allow copying argument data without upsetting the verifier.
 // This is not hashed when the key is used in the ratelimit_map.
+#if defined(GENERIC_UPROBE) || defined(GENERIC_URETPROBE) || defined(GENERIC_USDT)
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__uint(max_entries, 1); // will be resized by agent
+	__type(key, __u64);
+	__type(value, __u8[sizeof(struct ratelimit_key) + 128]);
+} ratelimit_heap SEC(".maps");
+
+FUNC_INLINE void *ratelimit_heap_get(void)
+{
+	__u64 key = get_current_pid_tgid();
+	void *val = map_lookup_elem(&ratelimit_heap, &key);
+	struct heap_ro_value *ro;
+	__u32 zidx = 0;
+
+	if (val)
+		return val;
+	ro = map_lookup_elem(&heap_ro_zero, &zidx);
+	if (!ro)
+		return 0;
+	if (map_update_elem(&ratelimit_heap, &key, ro, BPF_ANY))
+		return 0;
+	return map_lookup_elem(&ratelimit_heap, &key);
+}
+
+#else
 struct {
 	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
 	__uint(max_entries, 1);
 	__type(key, __u32);
 	__type(value, __u8[sizeof(struct ratelimit_key) + 128]);
 } ratelimit_heap SEC(".maps");
+
+FUNC_INLINE void *ratelimit_heap_get(void)
+{
+	__u32 zero = 0;
+
+	return map_lookup_elem(&ratelimit_heap, &zero);
+}
+
+#endif /* GENERIC_UPROBE || GENERIC_URETPROBE || GENERIC_USDT */
 
 #endif /* __RATELIMIT_MAPS_H__ */
