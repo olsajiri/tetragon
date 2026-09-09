bd19bd434d4b0d0fa227fe20a7055580bd432ad9
Jiri Olsa <jolsa@kernel.org>
2026-09-09 08:44:17 +0000

    tetragon: Switch string_postfix_maps_heap map to hash for uprobe/usdt
    The uprobe/usdt probes path in kernel do not disable preemption,
    we need to use hash instead of per-cpu heap.
    
    The uprobe and usdt sensor now create string_postfix_maps_heap as hash
    map with BPF_F_NO_PREALLOC and use it as shared map with 32768 entries.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- bpf/process/string_maps.h
+++ bpf/process/string_maps.h
@@ -233,12 +233,48 @@ struct {
 		});
 } string_postfix_maps SEC(".maps");
 
+#if defined(GENERIC_UPROBE) || defined(GENERIC_URETPROBE) || defined(GENERIC_USDT)
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__uint(max_entries, 1); // will be resized by agent
+	__type(key, __u64);
+	__type(value, struct string_postfix_lpm_trie);
+} string_postfix_maps_heap SEC(".maps");
+
+FUNC_INLINE void *string_postfix_maps_heap_get(void)
+{
+	__u64 key = get_current_pid_tgid();
+	void *val = map_lookup_elem(&string_postfix_maps_heap, &key);
+	struct heap_ro_value *ro;
+	__u32 zidx = 0;
+
+	if (val)
+		return val;
+	ro = map_lookup_elem(&heap_ro_zero, &zidx);
+	if (!ro)
+		return 0;
+	if (map_update_elem(&string_postfix_maps_heap, &key, ro, BPF_ANY))
+		return 0;
+	return map_lookup_elem(&string_postfix_maps_heap, &key);
+}
+
+#else
 struct {
 	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
 	__uint(max_entries, 1);
 	__type(key, __u32);
 	__type(value, struct string_postfix_lpm_trie);
 } string_postfix_maps_heap SEC(".maps");
+
+FUNC_INLINE void *string_postfix_maps_heap_get(void)
+{
+	__u32 zero = 0;
+
+	return map_lookup_elem(&string_postfix_maps_heap, &zero);
+}
+
+#endif /* GENERIC_UPROBE || GENERIC_URETPROBE || GENERIC_USDT */
 
 struct {
 	__uint(type, BPF_MAP_TYPE_ARRAY);
